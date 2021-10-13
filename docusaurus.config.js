eb132d4537b269b4a8172c98f1dda9caddd90b67
Takács László <laszlo@letmewebyou.com>
2021-10-13 22:31:40 +0300

    Disabled edit 2.0

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -24,13 +24,13 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          editUrl: null,
+          editUrl: 'null',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            null,
+            'null',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
